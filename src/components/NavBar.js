2f0df2edfe258d90024b640e234ff3da06389ec1
Dmitry Korochkin <abramslost@gmail.com>
2022-08-19 18:31:49 +0300

    first project -- add NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg';
+
+const NavBarStyled = styled.header`
+
+`;
+
+export const NavBar = () => {
+    <NavBarStyled>
+        <img src={logoImg} alt="logo" />
+    </NavBarStyled>
+};
